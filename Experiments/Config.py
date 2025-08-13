67ca186b45649d776804575d5346f61068d1fed4
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-08-13 11:41:18 +0530

    BUSI

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -40,7 +40,10 @@ resume_checkpoint = False  # or False
 #task_name = 'Clinic_exp1'
 #task_name = 'Covid_exp1'
 #task_name = 'BUSI_exp1'
-task_name = 'ISIC18_UNET'
+
+
+# task_name = 'ISIC18_UNET'
+task_name = 'BUSI_UNET'
 
 learning_rate = 1e-3
 batch_size = 128
@@ -52,13 +55,13 @@ batch_size = 128
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
 #model_name = 'MultiResUnet1_32_1.67'
-# model_name = 'UNeXt'
+model_name = 'UNeXt'
 # model_name = 'UNext_InceptionNext_MLFC'
 # model_name = 'UNext_CMRF'   # CMRF encoder
 # model_name = 'UNext_CMRF_enc_dec'  # CMRF encoder + decoder
 # model_name = 'UNext_CMRF_enc_MLFC'  # CMRF encoder + MLFC fusion
 # model_name = 'UNext_CMRF_enc_dec_MLFC'  # CMRF encoder + decoder + MLFC fusion
-model_name = 'UNext_CMRF_enc_CSSE'
+# model_name = 'UNext_CMRF_enc_CSSE'
 
 test_session = "session1"         #
 
@@ -77,10 +80,25 @@ test_session = "session1"         #
 # train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/train'
 # val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/val'
 # test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/test'
+
+# ISIC 18
 dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1'
 train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/train'
 val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/val'
 test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
+
+# Glas
+# dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1'
+# train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/train'
+# val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/val'
+# test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
+
+# BUSI
+dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_prepared/'
+train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_prepared/train'
+val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_prepared/val'
+test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_prepared/test'
+
 
 session_name       = 'session1'  #time.strftime('%m.%d_%Hh%M')
 save_path          = task_name +'/'+ model_name +'/' + session_name + '/'
