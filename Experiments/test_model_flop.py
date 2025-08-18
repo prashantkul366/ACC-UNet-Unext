057af1a338f5ecbffb9c00d0a261e60e64b15729
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-08-18 23:28:49 +0530

    CMRF PP

--- Experiments/test_model_flop.py
+++ Experiments/test_model_flop.py
@@ -42,7 +42,7 @@ from nets.archs.UNext_CMRF_enc_dec import UNext_CMRF_enc_dec
 from nets.archs.UNext_CMRF_enc_MLFC import UNext_CMRF_enc_MLFC
 from nets.archs.UNext_CMRF_enc_dec_MLFC import UNext_CMRF_enc_dec_MLFC
 from nets.archs.UNext_CMRF_enc_CSSE import UNext_CMRF_enc_CSSE
-from nets.archs.UNext_CMRF_PP import UNext_CMRF_PP
+from nets.archs.UNext_CMRF_PP import UNext_CMRF_PP_UNetPP
 
 ######################################################
 
@@ -279,7 +279,8 @@ if __name__ == '__main__':
         model = UNext_CMRF_enc_CSSE(n_channels=config.n_channels, n_classes=config.n_labels)
 
     elif model_type == 'UNext_CMRF_PP':
-        model = UNext_CMRF_PP(n_channels=config.n_channels, n_classes=config.n_labels)
+        # model = UNext_CMRF_PP(n_channels=config.n_channels, n_classes=config.n_labels)
+        model = UNext_CMRF_PP_UNetPP(n_channels=config.n_channels, n_classes=config.n_labels)
 
 
     elif model_type.split('_')[0] == 'MultiResUnet1':          
