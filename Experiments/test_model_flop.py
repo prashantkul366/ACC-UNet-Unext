1e6d08bf455334897d1ec7e9dde159d47f5911d2
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-09-23 10:21:05 +0530

    Loss test

--- Experiments/test_model_flop.py
+++ Experiments/test_model_flop.py
@@ -491,14 +491,14 @@ if __name__ == '__main__':
         print ("Let's use {0} GPUs!".format(torch.cuda.device_count()))
         model = nn.DataParallel(model, device_ids=[0,1,2,3])
 
-    model.load_state_dict(checkpoint['state_dict'])
+    # model.load_state_dict(checkpoint['state_dict'])
     print(model_type)
     print('Model loaded !')
 
     dummy_input = torch.randn(1, config.n_channels, config.img_size, config.img_size).cuda()
-    macs, params = profile(model, inputs=(dummy_input,), verbose=False)
-    model_params = params / 1e6
-    model_gflops = macs / 1e9
+    # macs, params = profile(model, inputs=(dummy_input,), verbose=False)
+    # model_params = params / 1e6
+    # model_gflops = macs / 1e9
     tf_test = ValGenerator(output_size=[config.img_size, config.img_size])
     test_dataset = ImageToImage2D(config.test_dataset, tf_test,image_size=config.img_size)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
@@ -602,8 +602,8 @@ if __name__ == '__main__':
     print(f"Sensitivity: {sensitivity_meter.avg * 100:.2f}%")
     print(f"Specificity: {specificity_meter.avg * 100:.2f}%")
     print(f"Accuracy: {accuracy_meter.avg * 100:.2f}%")
-    print(f"Params: {model_params:.2f} M")
-    print(f"GFLOPs: {model_gflops:.2f} G")
+    # print(f"Params: {model_params:.2f} M")
+    # print(f"GFLOPs: {model_gflops:.2f} G")
     print(f"Avg GPU Time/Image: {gpu_time_meter.avg:.4f} sec")
 
 
@@ -613,8 +613,8 @@ if __name__ == '__main__':
     fp.write(f"Sensitivity: {sensitivity_meter.avg * 100:.2f}%\n")
     fp.write(f"Specificity: {specificity_meter.avg * 100:.2f}%\n")
     fp.write(f"Accuracy: {accuracy_meter.avg * 100:.2f}%\n")
-    fp.write(f"Params (M): {model_params:.2f}\n")
-    fp.write(f"GFLOPs: {model_gflops:.2f}\n")
+    # fp.write(f"Params (M): {model_params:.2f}\n")
+    # fp.write(f"GFLOPs: {model_gflops:.2f}\n")
     fp.write(f"Avg GPU Time (s): {gpu_time_meter.avg:.4f}\n")
     
     
@@ -632,8 +632,8 @@ if __name__ == '__main__':
     'Sensitivity (%)': [float(sensitivity_meter.avg * 100)],
     'Specificity (%)': [float(specificity_meter.avg * 100)],
     'Accuracy (%)': [float(accuracy_meter.avg * 100)],
-    'Params (M)': [float(model_params)],
-    'GFLOPs': [float(model_gflops)],
+    # 'Params (M)': [float(model_params)],
+    # 'GFLOPs': [float(model_gflops)],
     'Avg GPU Time (s)': [float(gpu_time_meter.avg)],
     }
 
