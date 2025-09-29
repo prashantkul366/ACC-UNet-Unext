dea6b869c1ac9bc992d2058755051846dd1de60d
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-09-29 16:32:56 +0530

    GLas Unext

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -44,8 +44,8 @@ resume_checkpoint = False  # or False
 #task_name = 'BUSI_exp1'
 
 
-task_name = 'ISIC18_UNET'
-# task_name = 'Glas'
+# task_name = 'ISIC18_UNET'
+task_name = 'Glas'
 # task_name = 'BUSI_UNET'
 
 learning_rate = 1e-3
@@ -60,7 +60,7 @@ batch_size = 8
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
 # model_name = 'MultiResUnet1_32_1.67'
-# model_name = 'UNeXt'
+model_name = 'UNeXt'
 # model_name = 'UNext_InceptionNext_MLFC'
 # model_name = 'UNext_CMRF'   # CMRF encoder
 # model_name = 'UNext_CMRF_enc_dec'  # CMRF encoder + decoder
@@ -73,7 +73,7 @@ batch_size = 8
 # model_name = 'UNext_CMRF_GAB_wavelet'  # CMRF encoder + GAB fusion + wavelet
 # model_name = 'UNext_CMRF_GAB_wavelet_OD'  # CMRF encoder + GAB fusion + wavelet + ODConv
 # model_name = 'UNext_CMRF_BS_GS_Wavelet'  # CMRF encoder + BSConv + GS + SIM augmentation + wavelet
-model_name = 'UNext_CMRF_BSRB_GS_Wavelet'  # CMRF encoder + BSRB + GS + SIM augmentation + wavelet
+# model_name = 'UNext_CMRF_BSRB_GS_Wavelet'  # CMRF encoder + BSRB + GS + SIM augmentation + wavelet
 # model_name = 'UNext_CMRF_GS_Wavelet_OD'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet + ODConv
 # model_name = 'UNext_CMRF_GS_Wavelet_hd'  
 # model_name = 'UNext_CMRF_GS'  # CMRF encoder + Global Semnantic + SIM augmentation
@@ -109,10 +109,10 @@ test_session = "session1"         #
 
 
 # ISIC 18
-dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1'
-train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/train'
-val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/val'
-test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
+# dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1'
+# train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/train'
+# val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/val'
+# test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
 
 # Glas
 dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Glas'
