ab3f61bbe415ec64e29c208f7b22325ec99f737d
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-09-30 14:18:28 +0530

    CMRF GS wavelet glas

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -82,7 +82,7 @@ model_name = 'UNext_CMRF_GS_Wavelet'  # CMRF encoder + Global Semnantic + SIM au
 # model_name = 'UNext_CMRF_dense_skip'  # CMRF encoder + dense skip connection
 # model_name = 'U-KAN'
 
-test_session = "session1"         #
+test_session = "session2"         #
 
 
 # train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
@@ -95,10 +95,10 @@ test_session = "session1"         #
 # logger_path        = save_path + session_name + ".log"
 # visualize_path     = save_path + 'visualize_val/'
 
-dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2'
-train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/train'
-val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/val'
-test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/test'
+# dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2'
+# train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/train'
+# val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/val'
+# test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/test'
 
 
 # MoNuSeg
@@ -116,10 +116,10 @@ test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/te
 # test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
 
 # Glas
-# dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Glas'
-# train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/train'
-# val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
-# test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
+dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Glas'
+train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/train'
+val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
+test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
 
 # BUSI
 # dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_ACC/'
@@ -128,7 +128,7 @@ test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/te
 # test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_ACC/test'
 
 
-session_name       = 'session1'  #time.strftime('%m.%d_%Hh%M')
+session_name       = 'session2'  #time.strftime('%m.%d_%Hh%M')
 save_path          = task_name +'/'+ model_name +'/' + session_name + '/'
 model_path         = save_path + 'models/'
 tensorboard_folder = save_path + 'tensorboard_logs/'
