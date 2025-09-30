acb11a079f2fdea22d7e57ae35143e3c56bb0626
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-09-30 12:38:31 +0530

    CMRF GS wavelet glas

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -44,8 +44,8 @@ resume_checkpoint = False  # or False
 #task_name = 'BUSI_exp1'
 
 
-# task_name = 'ISIC18_UNET'
-task_name = 'Glas'
+task_name = 'ISIC18_UNET'
+# task_name = 'Glas'
 # task_name = 'BUSI_UNET'
 
 learning_rate = 1e-3
@@ -60,7 +60,7 @@ batch_size = 8
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
 # model_name = 'MultiResUnet1_32_1.67'
-# model_name = 'UNeXt'
+model_name = 'UNeXt'
 # model_name = 'UNext_InceptionNext_MLFC'
 # model_name = 'UNext_CMRF'   # CMRF encoder
 # model_name = 'UNext_CMRF_enc_dec'  # CMRF encoder + decoder
@@ -77,7 +77,7 @@ batch_size = 8
 # model_name = 'UNext_CMRF_BSRB_GS'  # CMRF encoder + BSRB + GS + SIM augmentation
 # model_name = 'UNext_CMRF_GS_Wavelet_OD'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet + ODConv
 # model_name = 'UNext_CMRF_GS_Wavelet_hd'  
-model_name = 'UNext_CMRF_GS'  # CMRF encoder + Global Semnantic + SIM augmentation
+# model_name = 'UNext_CMRF_GS'  # CMRF encoder + Global Semnantic + SIM augmentation
 # model_name = 'UNext_CMRF_GS_Wavelet'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet 
 # model_name = 'UNext_CMRF_dense_skip'  # CMRF encoder + dense skip connection
 # model_name = 'U-KAN'
@@ -95,10 +95,10 @@ test_session = "session1"         #
 # logger_path        = save_path + session_name + ".log"
 # visualize_path     = save_path + 'visualize_val/'
 
-# dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2'
-# train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/train'
-# val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/val'
-# test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/test'
+dataset_path = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2'
+train_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/train'
+val_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/val'
+test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic2/test'
 
 
 # MoNuSeg
@@ -116,10 +116,10 @@ test_session = "session1"         #
 # test_dataset = '/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic1/test'
 
 # Glas
-dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Glas'
-train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/train'
-val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
-test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
+# dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Glas'
+# train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/train'
+# val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
+# test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Glas/test'
 
 # BUSI
 # dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/BUSI_ACC/'
