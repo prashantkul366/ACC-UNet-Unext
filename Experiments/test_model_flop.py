3ffc7aeed711542f2bf63d55eb024bfdb64bee5f
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-10-06 12:21:15 +0530

    UNext CVC

--- Experiments/test_model_flop.py
+++ Experiments/test_model_flop.py
@@ -313,10 +313,10 @@ if __name__ == '__main__':
         model_type = config.model_name
         model_path = "./Kvasir-Seg/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"    
 
-    elif config.task_name =="CVC-ClinicDB   ":
+    elif config.task_name =="CVC-ClinicDB":
         test_num = 62
         model_type = config.model_name
-        model_path = "./Kvasir-Seg/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"    
+        model_path = "./CVC-ClinicDB/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"    
 
     #################################################################################################################
 
