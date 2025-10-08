76336abdf92c49eac90d43460f98c9a62df52e03
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-10-08 10:14:44 +0530

    Glas test

--- Experiments/test_model_flop.py
+++ Experiments/test_model_flop.py
@@ -323,6 +323,11 @@ if __name__ == '__main__':
         model_type = config.model_name
         model_path = "./BUSI/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"    
 
+    elif config.task_name =="Glas":
+        test_num = 130
+        model_type = config.model_name
+        model_path = "./Glas/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"    
+
     #################################################################################################################
 
 
