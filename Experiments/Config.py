7d077e580e077cf79fea69132b7e5ca6c14f8d69
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-10-13 17:51:03 +0530

    BUSI UNEXT TEST

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -52,9 +52,9 @@ resume_checkpoint = False  # or False
 # task_name = 'Glas'
 # task_name = 'ClinicDB'
 # task_name = 'ColonDB'
-# task_name = 'BUSI_80-20'
+task_name = 'BUSI_80-20'
 # task_name = 'STARE'
-task_name = 'DRIVE'
+# task_name = 'DRIVE'
 # task_name = 'BUSI_UNET'
 
 learning_rate = 1e-3
@@ -71,8 +71,8 @@ batch_size = 8
 # model_name = 'MultiResUnet1_32_1.67'
 
 
-# model_name = 'UNeXt'
-model_name = 'UNext_CMRF_GS_Wavelet'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet 
+model_name = 'UNeXt'
+# model_name = 'UNext_CMRF_GS_Wavelet'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet 
 
 
 # model_name = 'UNext_InceptionNext_MLFC'
@@ -162,10 +162,10 @@ test_session = "session1"         #
 
 
 # BUSI_80-20
-# dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20'
-# train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/train'
-# val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/test'
-# test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/test'
+dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20'
+train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/train'
+val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/test'
+test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/Dataset_BUSI_80_20/test'
 
 # STARE
 # dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/STARE_10_10'
@@ -174,10 +174,10 @@ test_session = "session1"         #
 # test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/STARE_10_10/test'
 
 # DRIVE
-dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE'
-train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/train'
-val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/test'
-test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/test'
+# dataset_path = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE'
+# train_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/train'
+# val_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/test'
+# test_dataset = '/content/drive/MyDrive/Prashant/research_datasets/DRIVE/test'
 
 
 
