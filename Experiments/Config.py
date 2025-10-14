aa43a0f394e16976c94f8689278823f3c58226f7
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-10-14 18:16:43 +0530

    CVC Swin

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -65,12 +65,12 @@ learning_rate = 1e-3
 batch_size = 8
 
 # model_name = 'ACC_UNet'
-# model_name = 'SwinUnet'
+model_name = 'SwinUnet'
 # model_name = 'SMESwinUnet'
 # model_name = 'UCTransNet'
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
-model_name = 'MultiResUnet1_32_1.67'
+# model_name = 'MultiResUnet1_32_1.67'
 
 
 # model_name = 'UNeXt'
