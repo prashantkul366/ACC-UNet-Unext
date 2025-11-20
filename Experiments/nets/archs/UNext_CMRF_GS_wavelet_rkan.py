34ad60d3c21cf4503183720e784ac66a197d9fa0
Prashant Kulkarni <prashantkul366@gmail.com>
2025-11-20 14:45:57 +0530

    RKAN

--- Experiments/nets/archs/UNext_CMRF_GS_wavelet_rkan.py
+++ Experiments/nets/archs/UNext_CMRF_GS_wavelet_rkan.py
@@ -29,8 +29,7 @@ import pywt
 
 # from Topformer import InjectionMultiSumCBR  # SIM module
 from nets.archs.Topformer import InjectionMultiSumCBR
-from .rkan import PadeRKAN, JacobiRKAN
-
+from nets.archs.kan_r import KANLinear, KAN
 
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
     """1x1 convolution"""
@@ -52,8 +51,8 @@ class shiftmlp(nn.Module):
         self.dim = in_features
         self.fc1 = nn.Linear(in_features, hidden_features)
         self.dwconv = DWConv(hidden_features)
-        # self.act = act_layer()
-        self.act = JacobiRKAN(3) 
+        self.act = act_layer()
+        # self.act = JacobiRKAN(3) 
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
@@ -213,6 +212,180 @@ class OverlapPatchEmbed(nn.Module):
         return x, H, W
 
 
+
+class DW_bn_relu(nn.Module):
+    def __init__(self, dim=768):
+        super(DW_bn_relu, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+        self.bn = nn.BatchNorm2d(dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        return x
+
+class KANLayer(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., no_kan=False):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.dim = in_features
+        
+        grid_size=5
+        spline_order=3
+        scale_noise=0.1
+        scale_base=1.0
+        scale_spline=1.0
+        base_activation=torch.nn.SiLU
+        grid_eps=0.02
+        grid_range=[-1, 1]
+
+        if not no_kan:
+            self.fc1 = KANLinear(
+                        in_features,
+                        hidden_features,
+                        grid_size=grid_size,
+                        spline_order=spline_order,
+                        scale_noise=scale_noise,
+                        scale_base=scale_base,
+                        scale_spline=scale_spline,
+                        base_activation=base_activation,
+                        grid_eps=grid_eps,
+                        grid_range=grid_range,
+                    )
+            self.fc2 = KANLinear(
+                        hidden_features,
+                        out_features,
+                        grid_size=grid_size,
+                        spline_order=spline_order,
+                        scale_noise=scale_noise,
+                        scale_base=scale_base,
+                        scale_spline=scale_spline,
+                        base_activation=base_activation,
+                        grid_eps=grid_eps,
+                        grid_range=grid_range,
+                    )
+            self.fc3 = KANLinear(
+                        hidden_features,
+                        out_features,
+                        grid_size=grid_size,
+                        spline_order=spline_order,
+                        scale_noise=scale_noise,
+                        scale_base=scale_base,
+                        scale_spline=scale_spline,
+                        base_activation=base_activation,
+                        grid_eps=grid_eps,
+                        grid_range=grid_range,
+                    )
+            # # TODO   
+            # self.fc4 = KANLinear(
+            #             hidden_features,
+            #             out_features,
+            #             grid_size=grid_size,
+            #             spline_order=spline_order,
+            #             scale_noise=scale_noise,
+            #             scale_base=scale_base,
+            #             scale_spline=scale_spline,
+            #             base_activation=base_activation,
+            #             grid_eps=grid_eps,
+            #             grid_range=grid_range,
+            #         )   
+
+        else:
+            self.fc1 = nn.Linear(in_features, hidden_features)
+            self.fc2 = nn.Linear(hidden_features, out_features)
+            self.fc3 = nn.Linear(hidden_features, out_features)
+
+        # TODO
+        # self.fc1 = nn.Linear(in_features, hidden_features)
+
+
+        self.dwconv_1 = DW_bn_relu(hidden_features)
+        self.dwconv_2 = DW_bn_relu(hidden_features)
+        self.dwconv_3 = DW_bn_relu(hidden_features)
+
+        # # TODO
+        # self.dwconv_4 = DW_bn_relu(hidden_features)
+    
+        self.drop = nn.Dropout(drop)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+    
+
+    def forward(self, x, H, W):
+        # pdb.set_trace()
+        B, N, C = x.shape
+
+        x = self.fc1(x.reshape(B*N,C))
+        x = x.reshape(B,N,C).contiguous()
+        x = self.dwconv_1(x, H, W)
+        x = self.fc2(x.reshape(B*N,C))
+        x = x.reshape(B,N,C).contiguous()
+        x = self.dwconv_2(x, H, W)
+        x = self.fc3(x.reshape(B*N,C))
+        x = x.reshape(B,N,C).contiguous()
+        x = self.dwconv_3(x, H, W)
+
+        # # TODO
+        # x = x.reshape(B,N,C).contiguous()
+        # x = self.dwconv_4(x, H, W)
+    
+        return x
+
+class KANBlock(nn.Module):
+    def __init__(self, dim, drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, no_kan=False):
+        super().__init__()
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim)
+
+        self.layer = KANLayer(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop, no_kan=no_kan)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x, H, W):
+        x = x + self.drop_path(self.layer(self.norm2(x), H, W))
+
+        return x
+
+
 class UNext_CMRF_GS_Wavelet_rKAN(nn.Module):
 
     ## Conv 3 + MLP 2 + shifted MLP
@@ -251,15 +424,25 @@ class UNext_CMRF_GS_Wavelet_rKAN(nn.Module):
 
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
 
-        self.block1 = nn.ModuleList([shiftedBlock(
-            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
-            sr_ratio=sr_ratios[0])])
+        # self.block1 = nn.ModuleList([shiftedBlock(
+        #     dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+        #     drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+        #     sr_ratio=sr_ratios[0])])
 
-        self.block2 = nn.ModuleList([shiftedBlock(
-            dim=embed_dims[2], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
-            sr_ratio=sr_ratios[0])])
+        # self.block2 = nn.ModuleList([shiftedBlock(
+        #     dim=embed_dims[2], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+        #     drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+        #     sr_ratio=sr_ratios[0])])
+
+        self.block1 = nn.ModuleList([KANBlock(
+            dim=embed_dims[1], 
+            drop=drop_rate, drop_path=dpr[0], norm_layer=norm_layer
+            )])
+
+        self.block2 = nn.ModuleList([KANBlock(
+            dim=embed_dims[2],
+            drop=drop_rate, drop_path=dpr[1], norm_layer=norm_layer
+            )])
         
         # ---- Global semantics (pool all encoders -> concat -> project) ----
         self.gs_size = img_size // 32                   # 224 -> 7
@@ -280,15 +463,25 @@ class UNext_CMRF_GS_Wavelet_rKAN(nn.Module):
         self.sim1 = InjectionMultiSumCBR(inp=16,  oup=16)   # inject at the t1 skip
 
 
-        self.dblock1 = nn.ModuleList([shiftedBlock(
-            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
-            sr_ratio=sr_ratios[0])])
+        # self.dblock1 = nn.ModuleList([shiftedBlock(
+        #     dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+        #     drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+        #     sr_ratio=sr_ratios[0])])
 
-        self.dblock2 = nn.ModuleList([shiftedBlock(
-            dim=embed_dims[0], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
-            sr_ratio=sr_ratios[0])])
+        # self.dblock2 = nn.ModuleList([shiftedBlock(
+        #     dim=embed_dims[0], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+        #     drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+        #     sr_ratio=sr_ratios[0])])
+
+        self.dblock1 = nn.ModuleList([KANBlock(
+            dim=embed_dims[1], 
+            drop=drop_rate, drop_path=dpr[0], norm_layer=norm_layer
+            )])
+
+        self.dblock2 = nn.ModuleList([KANBlock(
+            dim=embed_dims[0], 
+            drop=drop_rate, drop_path=dpr[1], norm_layer=norm_layer
+            )])
 
         self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],
                                               embed_dim=embed_dims[1])
