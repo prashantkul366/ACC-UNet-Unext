d117515f804bf539f32d6f15da002754ae2eeddd
Prashant Kulkarni <prashantkul366@gmail.com>
2025-12-08 15:01:08 +0530

    Proposed ISIC

--- Experiments/train_model.py
+++ Experiments/train_model.py
@@ -68,7 +68,11 @@ from nets.segmamba import SegMamba
 # from nets.segmamba_hybrid_gsc_KAN_PE_rm_fkan import SegMamba as Segmamba_hybrid_gsc_KAN_PE_rm_fkan
 # from nets.segmamba_hybrid_gsc_ds import SegMamba as Segmamba_hybrid_gsc_ds
 # from nets.segmamba_hybrid_gsc_KAN_PE_rm_fkan_ds import SegMamba as Segmamba_hybrid_gsc_KAN_PE_rm_fkan_ds
-# from nets.segmamba_hybrid_gsc_KAN_PE_ds import SegMamba as Segmamba_hybrid_gsc_KAN_PE_ds
+
+
+from nets.segmamba_hybrid_gsc_KAN_PE_ds import SegMamba as Segmamba_hybrid_gsc_KAN_PE_ds
+
+
 # from nets.segmamba_hybrid_gsc_KAN_PE_EffKan import SegMamba as segmamba_hybrid_gsc_KAN_PE_EffKan
 # from nets.segmamba_hybrid_gsc_KAN_PE_ds_flip import SegMamba as Segmamba_hybrid_gsc_KAN_PE_ds_flip
 # from nets.TransUnet_fKAN import TransUNet_KAN_fJNB
@@ -283,11 +287,11 @@ def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True, res
         model = UNext_InceptionNext_MLFC_fKAN(n_channels=config.n_channels, n_classes=config.n_labels)
 
 
-    elif model_type == 'Segmamba':
-        model = SegMamba(
-            in_chans=config.n_channels, out_chans=config.n_labels, depths=[2, 2, 2, 2],
-            feat_size=[48, 96, 192, 384], spatial_dims=3,)
-        lr = 1e-4
+    # elif model_type == 'Segmamba':
+    #     model = SegMamba(
+    #         in_chans=config.n_channels, out_chans=config.n_labels, depths=[2, 2, 2, 2],
+    #         feat_size=[48, 96, 192, 384], spatial_dims=3,)
+    #     lr = 1e-4
 
     # elif model_type == 'Segmamba_hybrid':
     #     model = SegMamba_hybrid(
@@ -343,11 +347,22 @@ def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True, res
     #         feat_size=[48, 96, 192, 384], spatial_dims=3,)
     #     lr = 1e-4
 
-    # elif model_type == 'Segmamba_hybrid_gsc_KAN_PE_ds':
-    #     model = Segmamba_hybrid_gsc_KAN_PE_ds(
-    #         in_chans=config.n_channels, out_chans=config.n_labels, depths=[2, 2, 2, 2],
-    #         feat_size=[48, 96, 192, 384], spatial_dims=3,)
-    #     lr = 1e-4    
+
+
+
+
+    elif model_type == 'Segmamba_hybrid_gsc_KAN_PE_ds':
+        model = Segmamba_hybrid_gsc_KAN_PE_ds(
+            in_chans=config.n_channels, out_chans=config.n_labels, depths=[2, 2, 2, 2],
+            feat_size=[48, 96, 192, 384], spatial_dims=3,)
+        lr = 1e-4   
+
+
+
+
+
+        
+         
 
     # elif model_type == 'segmamba_hybrid_gsc_KAN_PE_EffKan':
     #     model = segmamba_hybrid_gsc_KAN_PE_EffKan(
