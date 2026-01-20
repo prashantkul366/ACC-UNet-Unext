9c707435aa6a06221f6a1059f9f90ebe43b7ce7d
Prashant Kulkarni <prashantkul366@gmail.com>
2026-01-20 21:35:22 +0530

    Swin  BUSI 80-20 seed 43 Test

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -78,10 +78,10 @@ batch_size = 4
 # model_name = 'UCTransNet'
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
-# model_name = 'MultiResUnet1_32_1.67'
+model_name = 'MultiResUnet1_32_1.67'
 # model_name = 'U-KAN'
 
-model_name = 'UNeXt'
+# model_name = 'UNeXt'
 # model_name = 'UNext_CMRF_GS_Wavelet'  # CMRF encoder + Global Semnantic + SIM augmentation + wavelet 
 
 # model_name = 'UNext_CMRF_GS_Wavelet_rKAN'
