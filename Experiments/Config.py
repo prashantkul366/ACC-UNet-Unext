c84e528c2b971f6505a9e88eac0e3cb4089cf907
Prashant Kulkarni <prashantkul366@gmail.com>
2026-01-22 15:26:26 +0530

    SPLIT 75-10-15

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -157,8 +157,8 @@ img_size = 224 if model_name in models_224 else 256
 # test_session = "session102" 
 
 # test_session = "SPLIT_75_10_15_seed_42" 
-test_session = "SPLIT_75_10_15_seed_43" 
-# test_session = "SPLIT_75_10_15_seed_44" 
+# test_session = "SPLIT_75_10_15_seed_43" 
+test_session = "SPLIT_75_10_15_seed_44" 
 
 # train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
 # val_dataset = './datasets/'+ task_name+ '/Val_Folder/'
@@ -261,16 +261,16 @@ test_session = "SPLIT_75_10_15_seed_43"
 # test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/test'
 
 # BUSI_75-10-15_Seed 43
-dataset_path = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/'
-train_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/train'
-val_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/val'
-test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/test'
+# dataset_path = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/'
+# train_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/train'
+# val_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_43/val'
+# test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/test'
 
 # BUSI_75-10-15_Seed 44
-# dataset_path = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/'
-# train_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/train'
-# val_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/val'
-# test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/test'
+dataset_path = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/'
+train_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/train'
+val_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/split_seed_44/val'
+test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_15/test'
 
 
 
@@ -378,8 +378,8 @@ test_dataset = '/content/drive/MyDrive/Prashant/Dataset_BUSI_80_20/SPLIT_75_10_1
 # session_name       = 'session102'  #time.strftime('%m.%d_%Hh%M')
 
 # session_name       = 'SPLIT_75_10_15_seed_42'  #time.strftime('%m.%d_%Hh%M')
-session_name       = 'SPLIT_75_10_15_seed_43'  #time.strftime('%m.%d_%Hh%M')
-# session_name       = 'SPLIT_75_10_15_seed_44'  #time.strftime('%m.%d_%Hh%M')
+# session_name       = 'SPLIT_75_10_15_seed_43'  #time.strftime('%m.%d_%Hh%M')
+session_name       = 'SPLIT_75_10_15_seed_44'  #time.strftime('%m.%d_%Hh%M')
 
 save_path          = task_name +'/'+ model_name +'/' + session_name + '/'
 model_path         = save_path + 'models/'
