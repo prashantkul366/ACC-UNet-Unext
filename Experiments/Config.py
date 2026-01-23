4513187da785e6c40e9dbe1890c7ef155ff1af1f
Prashant Kulkarni <prashantkul366@gmail.com>
2026-01-23 21:55:35 +0530

    Multiresunert

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -72,13 +72,13 @@ learning_rate = 1e-3
 # batch_size = 8
 batch_size = 4
 
-model_name = 'ACC_UNet'
+# model_name = 'ACC_UNet'
 # model_name = 'SwinUnet'
 # model_name = 'SMESwinUnet'
 # model_name = 'UCTransNet'
 # model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
-# model_name = 'MultiResUnet1_32_1.67'
+model_name = 'MultiResUnet1_32_1.67'
 # model_name = 'U-KAN'
 
 # model_name = 'UNeXt'
