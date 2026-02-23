79e8e63013c342639080cc73462a0293307f8f58
Prashant Kulkarni <prashantkul366@gmail.com>
2026-02-23 09:23:05 +0530

    MultiresUnet Kvasir test

--- Experiments/Config.py
+++ Experiments/Config.py
@@ -82,9 +82,9 @@ batch_size = 2
 # model_name = 'SwinUnet'
 # model_name = 'SMESwinUnet'
 # model_name = 'UCTransNet'
-model_name = 'UNet_base'
+# model_name = 'UNet_base'
 # model_name = 'UNet_base_proto'
-# model_name = 'MultiResUnet1_32_1.67'
+model_name = 'MultiResUnet1_32_1.67'
 # model_name = 'U-KAN'
 
 # model_name = 'UNeXt'
