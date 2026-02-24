b34743fe5212734110f0f48bff19a4f12dc09b54
Prashant Kulkarni <prashantkul366@gmail.com>
2026-02-24 16:05:56 +0530

    Proposed covid

--- Experiments/train_model.py
+++ Experiments/train_model.py
@@ -317,7 +317,7 @@ def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True, res
                               batch_size=config.batch_size,
                               shuffle=True,
                               worker_init_fn=worker_init_fn,
-                              num_workers=8,
+                              num_workers=0,
                               pin_memory=True)
     
     print("Training Data Loaded!!")
@@ -325,7 +325,7 @@ def main_loop(batch_size=config.batch_size, model_type='', tensorboard=True, res
                             batch_size=config.batch_size,
                             shuffle=True,
                             worker_init_fn=worker_init_fn,
-                            num_workers=8,
+                            num_workers=0,
                             pin_memory=True)
 
     print("Val Data Loaded!!")
